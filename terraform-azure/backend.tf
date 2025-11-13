e8a13dfff158f7449674f75567a9e7954074d3bc
voom51 <voom51@yahoo.com>
2025-11-14 00:24:21 +0200

    a

--- terraform-azure/backend.tf
+++ terraform-azure/backend.tf
@@ -1,8 +1,9 @@
 terraform {
   backend "azurerm" {
-    resource_group_name  = "rg-terraform-test"
-    storage_account_name = "stterraform676436e2"
-    container_name       = "tfstate"
-    key                  = "terraform.tfstate"
+    resource_group_name   = "rg-terraform-test"
+    storage_account_name  = "stterraform676436e2"
+    container_name        = "tfstate"
+    key                   = "terraform.tfstate"
+    subscription_id       = "b3b74f5d-6623-4043-8d82-24bc7f981535"
   }
 }
--- terraform-azure/main.tf
+++ terraform-azure/main.tf
@@ -1,51 +1,20 @@
-# main.tf
-terraform {
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "~> 3.0"
-    }
-    random = {
-      source  = "hashicorp/random"
-      version = "~> 3.0"
-    }
-  }
-}
-
 provider "azurerm" {
-  features {}
+  features = {}
+  skip_provider_registration = true
 }
 
-# Resource Group (Terraform will compare this with the imported or existing RG)
 resource "azurerm_resource_group" "rg" {
-  name     = var.resource_group_name
-  location = var.location
+  name     = "rg-terraform-test"
+  location = "germanywestcentral"
 }
 
-# Generate a random suffix to avoid naming collisions for resources
-resource "random_id" "suffix" {
-  byte_length = 4
-}
-
-# Storage Account
-resource "azurerm_storage_account" "st" {
-  name                     = "stterraform${random_id.suffix.hex}"
-  resource_group_name      = azurerm_resource_group.rg.name
-  location                 = azurerm_resource_group.rg.location
-  account_tier             = "Standard"
-  account_replication_type = "LRS"
- #allow_blob_public_access = false
-}
-
-# Virtual Network
 resource "azurerm_virtual_network" "vnet" {
   name                = "vnet-terraform"
-  resource_group_name = azurerm_resource_group.rg.name
-  location            = azurerm_resource_group.rg.location
   address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
 }
 
-# Subnet
 resource "azurerm_subnet" "subnet" {
   name                 = "subnet-terraform"
   resource_group_name  = azurerm_resource_group.rg.name
@@ -53,19 +22,17 @@ resource "azurerm_subnet" "subnet" {
   address_prefixes     = ["10.0.1.0/24"]
 }
 
-# Useful outputs
-output "resource_group_name" {
-  value = azurerm_resource_group.rg.name
+resource "azurerm_storage_account" "st" {
+  name                     = "stterraform676436e2"
+  resource_group_name      = azurerm_resource_group.rg.name
+  location                 = azurerm_resource_group.rg.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+  enable_https_traffic_only = true
 }
 
-output "storage_account_name" {
-  value = azurerm_storage_account.st.name
-}
-
-output "vnet_name" {
-  value = azurerm_virtual_network.vnet.name
-}
-
-output "subnet_name" {
-  value = azurerm_subnet.subnet.name
+resource "azurerm_storage_container" "tfstate" {
+  name                  = "tfstate"
+  storage_account_name  = azurerm_storage_account.st.name
+  container_access_type = "private"
 }
