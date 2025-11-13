e20112e6618ddaa30fd2279f4781c41a822d3eb0
voom51 <voom51@yahoo.com>
2025-11-14 00:53:34 +0200

    a

--- terraform-azure/main.tf
+++ terraform-azure/main.tf
@@ -1,16 +1,14 @@
 terraform {
-  required_version = ">= 1.0.0"
+  required_version = ">= 0.12"
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = ">= 3.0.0"
     }
   }
 }
 
 provider "azurerm" {
   features {}
-  skip_provider_registration = true
 }
 
 resource "azurerm_resource_group" "rg" {
@@ -38,11 +36,11 @@ resource "azurerm_storage_account" "st" {
   location                 = azurerm_resource_group.rg.location
   account_tier             = "Standard"
   account_replication_type = "LRS"
-  enable_https_traffic_only = true
+  # eliminăm atribute care pot fi incompatibile cu provider vechi
 }
 
 resource "azurerm_storage_container" "tfstate" {
-  name                  = "tfstate"
-  storage_account_name  = azurerm_storage_account.st.name
+  name                 = "tfstate"
+  storage_account_name = azurerm_storage_account.st.name
   container_access_type = "private"
 }
